9cc24556b2646af180a91465de820ef5405f007c
Daniel Santiago Torres <RulosS29@outlook.es>
2024-04-16 22:48:41 -0500

    Create

--- src/main/java/co/edu/escuelaing/cvds/lab7/model/Employee.java
+++ src/main/java/co/edu/escuelaing/cvds/lab7/model/Employee.java
@@ -5,9 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
-import lombok.NonNull;
 import lombok.Setter;
-import org.antlr.v4.runtime.misc.NotNull;
 
 @Entity
 @Table(name = "EMPLOYEE")
@@ -16,19 +14,14 @@ import org.antlr.v4.runtime.misc.NotNull;
 public class Employee {
     @Id
     @Column(name = "EMPLOYEE_ID")
-    @NotNull
     private int Employee_id;
     @Column(name = "FIRST_NAME")
-    @NotNull
     private String First_name;
     @Column(name = "LAST_NAME")
-    @NotNull
     private String Last_name;
     @Column(name = "ROLE")
-    @NotNull
     private String Role;
     @Column(name = "SALARY")
-    @NotNull
     private Double Salary;
 
     public Employee(int Employee_id, String First_name, String Last_name, String Role, Double Salary) {
